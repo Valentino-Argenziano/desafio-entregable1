ba0a0787c51468c40bec9e16e9336c4e7d54f6d3
Valentino Argenziano <valeargenziano@hotmail.com>
2022-06-04 15:05:17 -0300

    Creamos repo

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+function solicitarNombre() {
+    alert("Buenas Noches! SOMOS LA AFIP!");
+    let nombre = prompt("Ingrese su nombre");
+    while (nombre === "") {
+        nombre = prompt("Ingrese su nombre");
+    }
+    return nombre;
+}
+
+let nombreCliente = solicitarNombre();
+let costo = parseFloat(prompt("Ingrese el precio de costo del producto"))
+
+while (true) {
+    if (costo != "" && !isNaN(costo)) {
+        alert("Muy bien!");
+        break;
+    } else {
+        alert("Tenes que escribir el precio de costo! (Solo el numero)");
+        break;
+    }
+}
+
+precioCosto = costo.toFixed(2);
+console.log("El precio de costo del producto a calcular es $" + precioCosto);
+
+function calcularIVA(costo) {
+    return (costo * 0.21)
+}
+
+let iva = calcularIVA(costo);
+precioIva = iva.toFixed(2);
+
+console.log("El iva del producto ingresado es $" + precioIva);
+
+function sumarIva(costo) {
+    return (costo + (costo * 0.21))
+}
+
+let preciConIva = sumarIva(costo);
+precioCon_Iva = preciConIva.toFixed(2);
+
+console.log("El precio del producto con IVA incluido es $" + precioCon_Iva);
